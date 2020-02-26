1c835bf2497be623d2b80529ab176b7f5bafc1f9
Corina Pelichowski <corina@danitt.com>
2020-02-26 17:17:58 +1000

    Working with Ajax and JSON

--- app.js
+++ app.js
@@ -1,38 +1,67 @@
-document.getElementById('button').addEventListener('click', loadData);
+document.getElementById('button1').addEventListener('click', loadCustomer);
 
-function loadData() {
-    // Create XHR Object
+document.getElementById('button2').addEventListener('click', loadCustomers);
+
+// Load Single Customer
+function loadCustomer(e) {
     const xhr = new XMLHttpRequest();
 
-    // OPEN
-    xhr.open('GET', 'data.txt', true);
-
-    // console.log('READYSTATE', xhr.readyState);
-
-    // Optional - Used for spinners/loaders
-    xhr.onprogress = function () {
-        console.log('READYSTATE', xhr.readyState);
-    }
+    xhr.open('GET', 'customer.json', true);
 
     xhr.onload = function () {
-        console.log('READYSTATE', xhr.readyState);
         if (this.status === 200) {
             // console.log(this.responseText);
-            document.getElementById('output').innerHTML = `<h1>${this.responseText}</h1>`;
+
+            const customer = JSON.parse(this.responseText);
+
+            const output = `
+                <ul>
+                    <li>ID: ${customer.id}</li>
+                    <li>Name: ${customer.name}</li>
+                    <li>Company: ${customer.company}</li>
+                    <li>Phone: ${customer.phone}</li>
+                </ul>
+            `;
+
+            document.getElementById('customer').innerHTML = output;
+
         }
-    }
-
-    // xhr.onreadystatechange = function () {
-    //     console.log('READYSTATE', xhr.readyState);
-    //     if (this.status === 200 && this.readyState === 4) {
-    //         console.log(this.responseText);
-    //     }
-    // }
-
-    xhr.onerror = function () {
-        console.log('Request error...');
     }
 
     xhr.send();
 }
 
+// Load Customers
+function loadCustomers(e) {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'customers.json', true);
+
+    xhr.onload = function () {
+        if (this.status === 200) {
+            // console.log(this.responseText);
+
+            const customers = JSON.parse(this.responseText);
+
+            let output = '';
+
+            customers.forEach(function (customer) {
+                output += `
+                <ul>
+                    <li>ID: ${customer.id}</li>
+                    <li>Name: ${customer.name}</li>
+                    <li>Company: ${customer.company}</li>
+                    <li>Phone: ${customer.phone}</li>
+                </ul>
+            `;
+            });
+
+
+
+            document.getElementById('customers').innerHTML = output;
+
+        }
+    }
+
+    xhr.send();
+}
